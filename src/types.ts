0de35452c5b72f40e0275df2db06c4e442fe1720
Vlad Pavlenko <pavlenkoliar@gmail.com>
2023-08-30 16:51:12 +0300

    remove clones

--- src/types.ts
+++ src/types.ts
@@ -27,17 +27,3 @@ export type MyContext = Context &
   };
 
 export type MyConversation = Conversation<MyContext>;
-
-export type MyContext2 = Context;
-
-export type MyContext3 = Context;
-
-export type MyContext4 = Context;
-
-export type MyContext5 = Context;
-
-export type MyContext6 = Context;
-
-export type MyContext7 = Context;
-
-

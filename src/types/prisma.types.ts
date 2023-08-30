0600d4137691081c257f56841e638033d3054480
Vlad Pavlenko <pavlenkoliar@gmail.com>
2023-08-30 18:13:52 +0300

    add AdminInputWithPerm type

--- src/types/prisma.types.ts
+++ src/types/prisma.types.ts
@@ -6,6 +6,10 @@ export type ChatInputWithoutSettings = Omit<Chat, 'settings'>;
 
 export type AdminInput = Admin;
 
+export type AdminInputWithPerm = Admin & {
+  has_del_perm: boolean;
+};
+
 export type SettingsInput = Settings;
 
 export type SettingsOnly = Omit<Settings, 'id' | 'chatId'> & {

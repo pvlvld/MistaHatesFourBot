2b63fa033c529938788d8e7f769eb6ab6b62d174
Vlad Pavlenko <pavlenkoliar@gmail.com>
2023-08-30 17:51:43 +0300

    init chatSettings helper

---
+++ src/helpers/chatSettings.ts
@@ -0,0 +1,5 @@
+import type { ChatSettings, MyContext } from '../types/grammy.types';
+
+export function getChatSettings(ctx: MyContext, settings: ChatSettings) {}
+
+export function setChatSettings(ctx: MyContext, settings: ChatSettings) {}

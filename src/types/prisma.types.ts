d08ecf7f407ff21c04337dced0a7a4a8eaf3f827
Vlad Pavlenko <pavlenkoliar@gmail.com>
2023-08-30 22:31:55 +0300

    move IntRange to utils.types

--- src/types/prisma.types.ts
+++ src/types/prisma.types.ts
@@ -1,4 +1,5 @@
 import { type Admin, type Chat, type Settings } from '@prisma/client';
+import { IntRange } from './utils.types';
 
 export type ChatInput = Chat;
 
@@ -15,15 +16,3 @@ export type SettingsInput = Settings;
 export type SettingsOnly = Omit<Settings, 'id' | 'chatId'> & {
   vote_percent: IntRange<0, 100>;
 };
-
-type Enumerate<
-  N extends number,
-  Acc extends number[] = []
-> = Acc['length'] extends N
-  ? Acc[number]
-  : Enumerate<N, [...Acc, Acc['length']]>;
-
-type IntRange<F extends number, T extends number> = Exclude<
-  Enumerate<T>,
-  Enumerate<F>
->;

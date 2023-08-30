c94274d1191bcc5d6a399e8f1b4766d09a9a40c7
Vlad Pavlenko <pavlenkoliar@gmail.com>
2023-08-30 17:59:25 +0300

    init prismas.types

---
+++ src/types/prisma.types.ts
@@ -0,0 +1,25 @@
+import { type Admin, type Chat, type Settings } from '@prisma/client';
+
+export type ChatInput = Chat;
+
+export type ChatInputWithoutSettings = Omit<Chat, 'settings'>;
+
+export type AdminInput = Admin;
+
+export type SettingsInput = Settings;
+
+export type SettingsOnly = Omit<Settings, 'id' | 'chatId'> & {
+  vote_percent: IntRange<0, 100>;
+};
+
+type Enumerate<
+  N extends number,
+  Acc extends number[] = []
+> = Acc['length'] extends N
+  ? Acc[number]
+  : Enumerate<N, [...Acc, Acc['length']]>;
+
+type IntRange<F extends number, T extends number> = Exclude<
+  Enumerate<T>,
+  Enumerate<F>
+>;

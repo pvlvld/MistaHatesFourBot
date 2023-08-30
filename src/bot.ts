253fb0bdbfdf993ca84cbdd6be3b43c822a8de95
Vlad Pavlenko <pavlenkoliar@gmail.com>
2023-08-30 20:57:23 +0300

    use i18n

--- src/bot.ts
+++ src/bot.ts
@@ -2,6 +2,7 @@ import { Bot, session } from 'grammy';
 import autoRetryTransformer from './transformers/auto-retry.transformer';
 import { MyContext } from './types/grammy.types';
 import textHandler from './handlers/text.handler';
+import i18n from './utils/i18n';
 
 if (!process.env.BOT_TOKEN) throw new Error('Token required');
 
@@ -19,6 +20,9 @@ bot.use(
   })
 );
 
+bot.use(i18n);
+
+// Handlers
 bot.on(':text', async (ctx) => textHandler(ctx));
 
 export default bot;

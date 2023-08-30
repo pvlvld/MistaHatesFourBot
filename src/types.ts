4042d9c68ac7df22556b82a82d94d0947f5589df
Vlad Pavlenko <pavlenkoliar@gmail.com>
2023-08-30 16:50:53 +0300

    update types

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,32 @@
-import { Context } from 'grammy';
+import type { Context } from 'grammy';
+import type { Chat } from '@grammyjs/types';
+import {
+  type Conversation,
+  type ConversationFlavor,
+} from '@grammyjs/conversations';
 
-export type MyContext = Context;
+export type ChatSettings = {};
+
+type MyGroupChat = Chat.GroupChat & {
+  settings: ChatSettings;
+};
+
+type MySupergroupChat = Chat.SupergroupChat & {
+  settings: ChatSettings;
+};
+
+export type MyChat =
+  | Chat.PrivateChat
+  | Chat.ChannelChat
+  | MyGroupChat
+  | MySupergroupChat;
+
+export type MyContext = Context &
+  ConversationFlavor & {
+    get chat(): MyChat | undefined;
+  };
+
+export type MyConversation = Conversation<MyContext>;
 
 export type MyContext2 = Context;
 

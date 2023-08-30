db0b736afba441911d3ff171e4b1f0f8f9b96e0c
Vlad Pavlenko <pavlenkoliar@gmail.com>
2023-08-30 21:05:40 +0300

    set i18n localeNegotiator

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -2,6 +2,8 @@ import type { MyContext } from '../types/grammy.types';
 import { I18n } from '@grammyjs/i18n';
 
 const i18n = new I18n<MyContext>({
+  // Remove if user more than one language
+  localeNegotiator: (ctx) => 'uk',
   defaultLocale: 'uk',
   directory: 'locales',
 });

2613da212d4e580b35a458dd63edf0e72cde9b15
Vlad Pavlenko <pavlenkoliar@gmail.com>
2023-08-31 18:56:40 +0300

    fix typo error

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -2,7 +2,7 @@ import type { MyContext } from '../types/grammy.types';
 import { I18n } from '@grammyjs/i18n';
 
 const i18n = new I18n<MyContext>({
-  // Remove if user more than one language
+  // Remove if used more than one language
   localeNegotiator: (ctx) => 'uk',
   defaultLocale: 'uk',
   directory: './src/locales',

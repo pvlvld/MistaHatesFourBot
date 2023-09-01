a972e74b0bb3c83971e83397153c6b551bc76d40
Vlad Pavlenko <pavlenkoliar@gmail.com>
2023-09-02 00:48:34 +0300

    add START_IMAGE

--- src/consts/media.ts
+++ src/consts/media.ts
@@ -3,3 +3,6 @@ export const SHOOT_IMAGE = process.env.SHOOT_IMAGE;
 
 if (!process.env.WELCOME_IMAGE) throw new Error('Welcome image required');
 export const WELCOME_IMAGE = process.env.WELCOME_IMAGE;
+
+if (!process.env.START_IMAGE) throw new Error('Start image required');
+export const START_IMAGE = process.env.START_IMAGE;

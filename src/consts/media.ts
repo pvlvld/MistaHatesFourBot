0003b03d852db3bdf622839404c08324592f554e
Vlad Pavlenko <pavlenkoliar@gmail.com>
2023-09-02 00:15:24 +0300

    add WELCOME_IMAGE

--- src/consts/media.ts
+++ src/consts/media.ts
@@ -1,2 +1,5 @@
 if (!process.env.SHOOT_IMAGE) throw new Error('Shoot image required');
 export const SHOOT_IMAGE = process.env.SHOOT_IMAGE;
+
+if (!process.env.WELCOME_IMAGE) throw new Error('Shoot image required');
+export const WELCOME_IMAGE = process.env.WELCOME_IMAGE;

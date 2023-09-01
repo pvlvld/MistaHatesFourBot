e4a0d4f78127cbe1854ebe09efddb80251d40b11
Vlad Pavlenko <pavlenkoliar@gmail.com>
2023-09-01 20:19:07 +0300

    init settings command

---
+++ src/commands/settings.command.ts
@@ -0,0 +1,17 @@
+import { MyContext } from '../types/grammy.types';
+import { menu_settings_chats } from '../ui/menus/settings.menu';
+
+export function settings_cmd(ctx: MyContext) {
+  if (ctx.chat?.type === 'private') {
+    ctx.reply('🕺🏻', { reply_markup: menu_settings_chats });
+    return;
+  }
+  ctx.reply(
+    'Налаштування доступні в [діалозі з ботом.](tg://resolve?domain=MistaHater4bot&start=start)',
+    {
+      parse_mode: 'Markdown',
+    }
+  );
+}
+
+export default settings_cmd;

e8444bc34eff00e21b87ae2b5685178c7fd349ab
Vlad Pavlenko <pavlenkoliar@gmail.com>
2023-09-02 00:48:26 +0300

    fix welcome image error msg

--- src/consts/media.ts
+++ src/consts/media.ts
@@ -1,5 +1,5 @@
 if (!process.env.SHOOT_IMAGE) throw new Error('Shoot image required');
 export const SHOOT_IMAGE = process.env.SHOOT_IMAGE;
 
-if (!process.env.WELCOME_IMAGE) throw new Error('Shoot image required');
+if (!process.env.WELCOME_IMAGE) throw new Error('Welcome image required');
 export const WELCOME_IMAGE = process.env.WELCOME_IMAGE;

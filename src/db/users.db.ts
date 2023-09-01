b37f569d9c3d22b3543bd3067fd9fce4180b2c88
Vlad Pavlenko <pavlenkoliar@gmail.com>
2023-09-01 17:15:38 +0300

    update to new schema

--- src/db/users.db.ts
+++ src/db/users.db.ts
@@ -4,7 +4,7 @@ export async function getUserFours(user_id: bigint, chat_id: bigint) {
   return (
     (
       await prisma.user.findUnique({
-        where: { user_id_chat_id: { user_id, chat_id } },
+        where: { userId_chatId: { userId: user_id, chatId: chat_id } },
         select: { fours: true },
       })
     )?.fours || 0

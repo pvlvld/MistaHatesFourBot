a2ee723a8ca4c21b7c8355aaedc2ced69baa9728
Vlad Pavlenko <pavlenkoliar@gmail.com>
2023-09-01 21:46:10 +0300

    add settings-vote menu

--- src/ui/menus/settings.menu.ts
+++ src/ui/menus/settings.menu.ts
@@ -1,6 +1,7 @@
 import { Menu, MenuRange } from '@grammyjs/menu';
 import { MyContext } from '../../types/grammy.types';
 import { getAdminChats } from '../../db/admins.db';
+import { getChatSettings, setChatSettings } from '../../helpers/chatSettings';
 
 export const menu_settings_chats = new Menu<MyContext>('settings-chats')
   .dynamic(async (ctx) => {
@@ -13,7 +14,7 @@ export const menu_settings_chats = new Menu<MyContext>('settings-chats')
 
     for (const chat of chats) {
       range
-        .submenu(chat.name, 'settings-main', () =>
+        .submenu(chat.name, 'settings-list', () =>
           ctx.editMessageText(`Налаштування: "${chat.name}"\nid: ${chat.id}`)
         )
         .row();
@@ -34,3 +35,50 @@ const settings_list = new Menu<MyContext>('settings-list').dynamic(
     return range;
   }
 );
+
+const settings_vote = new Menu<MyContext>('settings-vote', {
+  autoAnswer: false,
+}).dynamic(async (ctx) => {
+  if (!ctx.msg?.text) return;
+
+  const range = new MenuRange<MyContext>();
+  const chat_id = BigInt(ctx.msg.text.substring(ctx.msg.text.lastIndexOf('-')));
+  const chat_settings = await getChatSettings(chat_id);
+
+  range
+    .text(
+      `${ctx.t('vote')} ${
+        chat_settings.vote_enable
+          ? ctx.t('status-emoji-on')
+          : ctx.t('status-emoji-off')
+      }`,
+      async (ctx) => {
+        chat_settings.vote_enable = !chat_settings.vote_enable;
+        await setChatSettings(chat_id, chat_settings);
+        ctx.menu.update();
+      }
+    )
+    .row()
+    .text(`${chat_settings.vote_percent}%`, async (ctx) => {
+      await ctx.reply(`Надішліть бажаний відсоток.\nid: ${chat_id}`, {
+        reply_markup: { force_reply: true },
+        parse_mode: 'Markdown',
+      });
+
+      await ctx.conversation.enter('votePersentConversation');
+    })
+    .row()
+    .text('?', async (ctx) =>
+      ctx.answerCallbackQuery({
+        show_alert: true,
+        text: ctx.t('percent-explanation'),
+      })
+    )
+    .row()
+    .back(ctx.t('back'));
+
+  return range;
+});
+
+menu_settings_chats.register(settings_list);
+settings_list.register(settings_vote);

e1258bab2acfee4742c69c391c6615e750c53bbb
Vlad Pavlenko <pavlenkoliar@gmail.com>
2023-09-01 16:13:35 +0300

    use AdminsTransformer

--- src/helpers/getChatAdmins.ts
+++ src/helpers/getChatAdmins.ts
@@ -1,8 +1,7 @@
 import * as Cache from '../cache/cache';
 import { MyGroupTextContext } from '../types/grammy.types';
 import * as db from '../db/chats.db';
-import { AdminInputWithPerm } from '../types/prisma.types';
-import { ChatMemberAdministrator, ChatMemberOwner } from '@grammyjs/types';
+import AdminsTransformer from './AdminsTransformer';
 
 async function getChatAdmins(ctx: MyGroupTextContext) {
   const chat_id = BigInt(ctx.chat.id);
@@ -13,28 +12,11 @@ async function getChatAdmins(ctx: MyGroupTextContext) {
   // api
   let api_admins = await ctx.getChatAdministrators().catch((e) => {});
   if (typeof api_admins === 'object') {
-    return formatAPIAdmins(api_admins);
+    return AdminsTransformer.APItoUsable(api_admins);
   }
   // db
   admins = new Set(await db.getChatAdmins(chat_id));
   return admins;
-}
-
-function formatAPIAdmins(
-  admins: (ChatMemberOwner | ChatMemberAdministrator)[]
-): Set<AdminInputWithPerm> {
-  const formatedAdmins = new Set([]) as Set<AdminInputWithPerm>;
-
-  admins.forEach((admin) => {
-    formatedAdmins.add({
-      id: BigInt(admin.user.id),
-      name: `${admin.user.first_name} ${admin.user.last_name || ''}`.trim(),
-      username: admin.user.username || null,
-      has_del_perm: admin.status === 'creator' || admin.can_delete_messages,
-    });
-  });
-
-  return formatedAdmins;
 }
 
 export default getChatAdmins;

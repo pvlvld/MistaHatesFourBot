d7028430e4d5583f77e40b8663cd8b0a91aa1cd7
Vlad Pavlenko <pavlenkoliar@gmail.com>
2023-09-01 20:19:28 +0300

    init settings menu

---
+++ src/ui/menus/settings.menu.ts
@@ -0,0 +1,23 @@
+import { Menu, MenuRange } from '@grammyjs/menu';
+import { MyContext } from '../../types/grammy.types';
+import { getAdminChats } from '../../db/admins.db';
+
+export const menu_settings_chats = new Menu<MyContext>('settings-chats')
+  .dynamic(async (ctx) => {
+    const chats = await getAdminChats(BigInt(ctx.from?.id || -1));
+    const range = new MenuRange<MyContext>();
+
+    if (Object.keys(chats).length === 0) {
+      return range.text('Чатів не знайдено').row();
+    }
+
+    for (const chat of chats) {
+      range
+        .submenu(chat.name, 'settings-main', () =>
+          ctx.editMessageText(`Налаштування: "${chat.name}"\nid: ${chat.id}`)
+        )
+        .row();
+    }
+    return range;
+  })
+  .url('Додати бота в чат', 't.me/MistaHater4bot?startgroup');

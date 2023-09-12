c74c235c80ae7cecf6cd0d7a3bd6448cf67e7e7b
Vlad Pavlenko <pavlenkoliar@gmail.com>
2023-09-12 12:50:03 +0300

    send start_menu

--- src/commands/start.command.ts
+++ src/commands/start.command.ts
@@ -1,5 +1,6 @@
 import { START_IMAGE } from '../consts/media';
 import { MyContext } from '../types/grammy.types';
+import { start_menu } from '../ui/menus/start.menu';
 
 export function start_cmd(ctx: MyContext) {
   if (ctx.chat?.type !== 'private') {
@@ -8,6 +9,7 @@ export function start_cmd(ctx: MyContext) {
 
   ctx.replyWithPhoto(START_IMAGE, {
     caption: ctx.t('start'),
+    reply_markup: start_menu,
   });
 }
 
